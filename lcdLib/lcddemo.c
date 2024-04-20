3225dc302210d0f3e2ca764fd906a58efeac81ca
Jorge Garcia <jhgarcia5@miners.utep.edu>
2024-04-20 15:44:17 -0600

    First iteration trying to change screen color

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -14,10 +14,53 @@ main()
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_MAGENTA);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  short centerCol = screenWidth >> 1;
+  short centerRow = screenHeight >> 1;
+
+  short size = 20;
+  //Top part of heart
+  for (short i = 0; i < size/2; i++){
+    drawPixel(centerCol+i, centerRow - i, COLOR_PINK);
+    drawPixel(centerCol-i, centerRow -i, COLOR_PINK);
+  }
+
+  //TOP PART, edges
+
+  for (short i=0; i < size/2; i++){
+    drawPixel(centerCol-(size/2)-i, centerRow-(size/2)+i,COLOR_PINK);
+    drawPixel(centerCol+(size/2)+i, centerRow-(size/2)+i, COLOR_PINK);      
+  }
+
+  //BOTTOM PART
+
+  for (short i = 0; i < size; i++){
+    drawPixel(centerCol - size + i, centerRow + i, COLOR_PINK);
+    drawPixel(centerCol + size - i, centerRow + i, COLOR_PINK);
+  }
+
+  
+  
+  /*
+  int x = 0;
+  int y = 0;
+  int speed = 6;
+  for (short i = 0; i < screenWidth; i++){
+    drawPixel(x, centerRow - y, COLOR_PINK);
+    drawPixel(x, centerRow - speed, COLOR_GREEN);
+    x++;
+    y += speed;
+    speed -= 1;
+    if (y <= 0)
+      speed =- speed;
+  }
+
+  */
+  
   
 }
